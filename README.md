5aa7600a854abdc25137af6e128da75493bc775c
xushao-fj <xushaomin_it@163.com>
2020-03-13 23:54:32 +0800

    feat: netty执行流程

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # netty框架学习 2020-03-13
 # netty hello 2020-03-13
+- netty执行流程分析与重要组件介绍
+- netty回调与Channel执行流程分析
+>- 主要看TestHttpServerHandler中复写父类的方法
--- src/main/java/netty/firstexample/TestHttpServerHandler.java
+++ src/main/java/netty/firstexample/TestHttpServerHandler.java
@@ -7,9 +7,12 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  * @author xsm
  * @Date 2020/3/13 22:28
+ * SimpleChannelInboundHandler 进来请求的处理
  */
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
@@ -21,7 +24,21 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
      */
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
+
+        // 查看请求实例
+        System.out.println(httpObject.getClass());
+
+        // 获取远程地址
+        System.out.println(channelHandlerContext.channel().remoteAddress());
+
         if (httpObject instanceof HttpRequest) {
+            HttpRequest httpRequest = (HttpRequest) httpObject;
+            System.out.println("请求方法名:" + httpRequest.method().name());
+            URI uri = new URI(httpRequest.getUri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                System.out.println("请求favicon.ico");
+                return;
+            }
             ByteBuf content = Unpooled.copiedBuffer("Hello world", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
             // http 响应头
@@ -29,6 +46,45 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
             // 响应内容
             response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
             channelHandlerContext.writeAndFlush(response);
+            // 关闭链接, 但是需要判断请求是http1.0(短连接), 还是http1.1(长连接), 如果是http1.1才需要关闭链接
+            // 关闭链接, 但是需要判断请求是http1.0(短连接), 还是http1.1(长连接), 如果是http1.1才需要关闭链接
+            channelHandlerContext.channel().close();
         }
+    }
+
+    // 以上为第一个hello world 程序
+
+
+    // 以下复写方法
+    // 以下方法可以看出netty接收请求的执行流程
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel active");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel registered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler added");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel inactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel unregistered");
+        super.channelUnregistered(ctx);
     }
 }
